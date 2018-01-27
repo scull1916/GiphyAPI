6f35e7bbdfe9c5370081a780e8ad022fcda5325f
scull1916 <kpscully@gmail.com>
2018-01-26 20:55:27 -0500

    buttons set up including user input converted into button and dynamically added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,41 @@
 var topics = 
 [
-	"astroboy",
-	"miyazaki",
+	"black books",
+	"doctor who",
+	"dougal",
+	"father ted",
+	"flight of the conchords",
+	"it crowd",
 	"phineas and ferb",
-	"simpsons",
+	"simpsons"
+	
+];
+var userPick = "I got picked!";
+var addShow = "";
 
-]
+
+//Dynamically add buttons with content from the topics array
+for (i=0; i<topics.length; i++)
+{
+		
+		$("<button>").appendTo(".button").text(topics[i]).addClass("btn btn-outline-primary").attr("value", topics[i]);
+
+
+}
+
+//Receive input from the user and use the input to dynamically add a button to the other buttons
+$("#submitBtn").on("click", function(event)
+{
+	event.preventDefault();
+
+	addShow = $("#userAdd").val().trim();
+
+
+
+	$("<button>").appendTo(".button").text(addShow).addClass("btn btn-outline-primary").attr("value", addShow);
+	topics.push(addShow);
+	
+
+
+});
+
